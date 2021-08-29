16ac8075f270d1b15b880fc17ff49ff068d0e2ad
Tony Golding <tpg@blueyonder.co.uk>
2021-08-29 13:48:49 +0100

    Project initialised.

---
+++ src/test/java/com/tpg/cat/reviews/ReviewsSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpg.cat.reviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewsSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
